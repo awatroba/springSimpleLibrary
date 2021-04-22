a8a3fb1bb5f464be19b0efb7b59b856680d80550
Angelika <watroba.angelika@gmail.com>
2021-04-22 13:19:40 +0200

    Create commit

---
+++ src/test/java/pk/ztp/springlibrary/ApplicationStarterTests.java
@@ -0,0 +1,13 @@
+package pk.ztp.springlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStarterTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
