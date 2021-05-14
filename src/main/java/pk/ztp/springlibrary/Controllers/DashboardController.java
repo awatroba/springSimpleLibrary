a7794e244db718fc6e2aadf522c3bab25d3bdd90
Angelika <watroba.angelika@gmail.com>
2021-05-14 18:38:15 +0200

    error handler correct

--- src/main/java/pk/ztp/springlibrary/Controllers/DashboardController.java
+++ src/main/java/pk/ztp/springlibrary/Controllers/DashboardController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pk.ztp.springlibrary.Exception.BookExistException;
+import pk.ztp.springlibrary.Exception.BookNotFoundException;
 import pk.ztp.springlibrary.Services.DashboardService;
 import pk.ztp.springlibrary.Services.ServiceResponse;
 import pk.ztp.springlibrary.beans.Book;
@@ -20,34 +21,50 @@ public class DashboardController {
     }
 
     @PostMapping("/saveBook")
-    public ResponseEntity<Object> addBook(@RequestBody Book book) throws BookExistException {
-        dashboardService.addBook(book);
-        ServiceResponse<Book> response = new ServiceResponse<Book>("success", book);
+    public ResponseEntity<Object> addBook(@RequestBody Book book){
+        ServiceResponse<Book> response;
+        try {
+            dashboardService.addBook(book);
+            response = new ServiceResponse<Book>("success", book);
+        } catch (BookExistException e) {
+            response = new ServiceResponse<Book>("Error: "+e.getMessage(), book);
+        }
         return new ResponseEntity<Object>(response, HttpStatus.OK);
     }
 
     @GetMapping("/getBooks")
     public ResponseEntity<Object> getAllBooks() {
-        ServiceResponse<List<Book>> response =
-                new ServiceResponse<>("success", dashboardService.getBooks());
+        ServiceResponse<List<Book>> response;
+        response= new ServiceResponse<>("success", dashboardService.getBooks());
         return new ResponseEntity<Object>(response, HttpStatus.OK);
     }
     @DeleteMapping(value = "/dashboard/{bookId}")
     public ResponseEntity<Object> deleteBook(@PathVariable int bookId) {
-        Book deleteBook=dashboardService.deleteBook(bookId);
-        ServiceResponse<Book> response =
-                new ServiceResponse<Book>("success",deleteBook );
+        Book deleteBook= null;
+        ServiceResponse<Book> response;
+        try {
+            deleteBook = dashboardService.deleteBook(bookId);
+            response=new ServiceResponse<Book>("success",deleteBook );
+        } catch (BookNotFoundException e) {
+            e.printStackTrace();
+            response=new ServiceResponse<Book>("Error: "+e.getMessage(),deleteBook );
+        }
         return new ResponseEntity<Object>(response, HttpStatus.OK);
     }
 
     @GetMapping(value = "/dashboard/{bookId}")
     public ResponseEntity<Object> getBookById(@PathVariable int bookId) {
-        Book searchBook=dashboardService.getBookById(bookId);
-        ServiceResponse<Book> response =
-                new ServiceResponse<Book>("success",searchBook );
+        Book searchBook= null;
+        ServiceResponse<Book> response;
+        try {
+            searchBook = dashboardService.getBookById(bookId);
+            response=new ServiceResponse<Book>("success",searchBook );
+        } catch (BookNotFoundException e) {
+            e.printStackTrace();
+            response=new ServiceResponse<Book>("Error: "+e.getMessage(),searchBook );
+        }
         return new ResponseEntity<Object>(response, HttpStatus.OK);
     }
-
    /*
 
 
--- src/main/java/pk/ztp/springlibrary/Exception/BookNotFoundException.java
+++ src/main/java/pk/ztp/springlibrary/Exception/BookNotFoundException.java
@@ -1,6 +1,6 @@
 package pk.ztp.springlibrary.Exception;
 
-public class BookNotFoundException  extends RuntimeException{
+public class BookNotFoundException  extends Exception{
     public BookNotFoundException(int id) {
         super("Book with id "+id+" not found!");
     }
